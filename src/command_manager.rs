f221db050e21765093bfd33d620fd59386c3545b
tch <tristan.chretien@tenacy.io>
2024-02-09 23:12:35 +0100

    feat!: add serd

--- src/command_manager.rs
+++ src/command_manager.rs
@@ -1,8 +1,6 @@
-use std::{
-    error::Error,
-    process::{Command, Output},
-    sync::mpsc::Sender,
-};
+use serde_json;
+use std::collections::HashMap;
+use std::{process::Command, sync::mpsc::Sender};
 
 pub struct CommandManager {
     pub current_dir: String,
@@ -13,13 +11,15 @@ impl CommandManager {
         Self { current_dir }
     }
 
-    fn handle_output(&self, output: Result<Output, std::io::Error>, channel: Sender<String>) {
+    fn handle_output(
+        &self,
+        output: Result<HashMap<&str, String>, std::io::Error>,
+        channel: Sender<String>,
+    ) {
         match output {
             Ok(data) => {
-                // println!("Command exec: <{}>",);
-                channel
-                    .send(String::from_utf8(data.stdout).unwrap())
-                    .unwrap();
+                let serialized_data = serde_json::to_string(&data).unwrap();
+                channel.send(serialized_data).unwrap();
             }
             Err(err) => {
                 eprintln!("Error running command:\n{}", err);
@@ -28,16 +28,49 @@ impl CommandManager {
     }
 
     pub fn directories_stats(&self, channel: Sender<String>) {
-        let args = &["-c", "ls -l | head -n 10"];
-        let output = self.exec("sh", args);
-        self.handle_output(output, channel);
+        let commands = [
+            (
+                "CDS",
+                &["-c", &format!("ls -l {} | head -n 10", self.current_dir)],
+            ),
+            (
+                "NSD",
+                &[
+                    "-c",
+                    &format!("find {} -mindepth 1 -type d | wc -l", self.current_dir),
+                ],
+            ),
+            ("SD", &["-c", &format!("du -sh {}", self.current_dir)]),
+            (
+                "NF",
+                &["-c", &format!("find {} -type f | wc -l", self.current_dir)],
+            ),
+        ];
+
+        let mut result_output = HashMap::new();
+
+        for (key, args) in commands {
+            if let Ok(output) = self.get_command_output("sh", args) {
+                result_output.insert(key, output);
+            }
+        }
+        self.handle_output(Ok(result_output), channel);
     }
 
-    fn exec(&self, cmd: &str, args: &[&str]) -> Result<std::process::Output, std::io::Error> {
-        return Command::new(cmd)
-            .args(args)
-            .current_dir(&self.current_dir)
-            .output();
+    fn get_command_output(&self, command: &str, args: &[&str]) -> Result<String, std::io::Error> {
+        let output = Command::new(command).args(args).output()?;
+
+        if output.status.success() {
+            Ok(String::from_utf8_lossy(&output.stdout).into_owned())
+        } else {
+            Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                format!(
+                    "Command failed: {}",
+                    String::from_utf8_lossy(&output.stderr)
+                ),
+            ))
+        }
     }
 
     pub fn controller(&self, channel: Sender<String>, job_id: &str) {
